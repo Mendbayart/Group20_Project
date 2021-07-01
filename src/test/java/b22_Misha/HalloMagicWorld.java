ed6503936fef209ed9d4fd87cbc098a349ef00f1
Misha Antic <mishaantic17@gmail.com>
2021-06-30 22:35:16 -0400

    Misha's 1st commit in his branch

--- src/test/java/b22_Misha/HalloMagicWorld.java
+++ src/test/java/b22_Misha/HalloMagicWorld.java
@@ -3,5 +3,6 @@ package b22_Misha;
 public class HalloMagicWorld {
     public static void main(String[] args) {
         System.out.println("Hello magic world!");
+        System.out.println("New test");
     }
 }
